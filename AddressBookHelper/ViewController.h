ee72e41f3b85150090c35255f706d187dd10d0a5
Canymebee <Canymebee@163.com>
2014-11-03 18:54:27 +0800

    Initial Commit

---
+++ AddressBookHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AddressBookHelper
+//
+//  Created by Rain on 14/11/3.
+//  Copyright (c) 2014年 Canymebee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AddressBookHelper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AddressBookHelper
+//
+//  Created by Rain on 14/11/3.
+//  Copyright (c) 2014年 Canymebee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
